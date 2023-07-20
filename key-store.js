36c120f01f536e9bb037e6cd43f8e487b48de4b8
makwasi1 <crismakwasi12@gmail.com>
2023-07-20 10:34:08 +0300

    generate and validate apikeys using fs

--- key-store.js
+++ key-store.js
@@ -1,11 +1,19 @@
 const VALID_KEYS_PATH = __dirname + '/valid-keys.txt';
 const fs = require('fs');
 const shortid = require('shortid');
-// To generate a unique API KEY, use shortid.generate()
 const LINE_ENDING = require('os').EOL;
 
-
 module.exports = function (req, res) {
+    //generate a unique API KEY
+    const API_KEY = shortid.generate();
 
+    //append the key to the file
+    fs.appendFile(VALID_KEYS_PATH, API_KEY + LINE_ENDING, function (err) {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error generating key');
+        } else {
+            res.status(201).send({'apikey': API_KEY});
+        }
+    });
 };
-
--- middleware.js
+++ middleware.js
@@ -1,5 +1,30 @@
 const VALID_KEYS_PATH = __dirname + '/valid-keys.txt';
+const fs = require('fs');
 
 module.exports = function (req, res, next) {
+    // Get the API key from the 'x-api-key' header
+    const API_KEY = req.headers['x-api-key'];
+ 
+    if (!API_KEY) {
+        // API key is missing, respond with 400 Bad Request
+        return res.status(400).send('API Key is missing');
+    }
 
+    fs.readFile(VALID_KEYS_PATH, function (err, data) {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error reading file');
+        }
+
+        const validKeys = data.toString().split('\n'); // Split the file by new line
+        const trimmedAPI_KEY = API_KEY.trim(); // Trim leading and trailing whitespace
+
+        if (validKeys.includes(trimmedAPI_KEY)) {
+            // API key is valid, call the next middleware
+            next();
+        } else {
+            // Invalid API key, respond with 401 Unauthorized
+            res.status(401).send('Invalid API Key');
+        }
+    });
 };
